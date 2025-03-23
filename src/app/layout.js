5d28335bdc456e3981b2495b58a6ee54bd512ec2
nha1410 <thanhnha141001@gmail.com>
2025-03-24 01:25:30 +0700

    resolve conflict

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
-import Footer from "./components/Footer";
+import Footer from "../components/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
